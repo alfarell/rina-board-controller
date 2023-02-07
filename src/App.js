880ee773f0cdd04253942a5359f432815e3e4328
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2023-02-07 21:22:39 +0700

    initial project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {BluetoothScreen, HomeScreen} from './Pages';
+
+function App() {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Connect Bluetooth" component={BluetoothScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/Pages/BluetoothScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+const BluetoothScreen = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+export default BluetoothScreen;
